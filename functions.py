23aa7fc944d67ab00ae4900dd8dfa1365cf5fd11
INAMUL_FAYAZ_WAIDA15 <inamfw15@gmail.com>
2024-09-15 23:58:38 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,24 @@
+# def name(fname, lname):
+#     print("Hello,", fname,lname)
+
+# name("Sam", "Wilson")
+
+def Gmean(a, b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isGreater(a,b):
+    if(a>b):
+        print("a is greater than b")
+    else:
+        print('a is less than8 b')
+
+def isLesser(a,b):
+    pass
+
+a = int(input("a: "))
+b = int(input("b: "))
+
+Gmean(a, b)
+isGreater(a, b)
+isLesser(a, b)
