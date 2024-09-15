4118ea753ea0f7fd606333711af893dab0013cd9
INAMUL_FAYAZ_WAIDA15 <inamfw15@gmail.com>
2024-09-15 21:08:16 +0530

    Create looping.py

---
+++ looping.py
@@ -0,0 +1,28 @@
+#iteration over a string
+
+name = 'Inaam'
+for i in name:
+    print(i, end = ",")
+
+# iterating over a List
+
+colors = ["red","green","blue","Yellow"]
+for x in colors:
+    print(x)
+
+#using range()
+
+for i in range(5):
+    print(i)
+
+for j in range(5,10):
+    print(j)
+
+for k in range(9):
+    print(k+1)
+
+for j in range(1,12,3):
+    print(j)
+
+# The `range(1, 12, 3)` function call generates a sequence of numbers starting from 1 up to,
+# but not including, 12 with a step size of 3.
