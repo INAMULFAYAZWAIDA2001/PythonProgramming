4d11c88f2c3adca25b4720717b42c9456361511b
INAMUL_FAYAZ_WAIDA15 <inamfw15@gmail.com>
2024-09-17 00:55:55 +0530

    Create fstring.py

---
+++ fstring.py
@@ -0,0 +1,12 @@
+name = input("Enter name: ")
+age = int(input("Enter Age: "))
+
+print(f"Hello, My name is {name} and I am {age} years old")
+
+if age>18:
+    print('Remarks: Eligible')
+else:
+    print("Remarks: Not Eligible")
+
+
+print(f"{2*30}") #we can use it in single statement as well
