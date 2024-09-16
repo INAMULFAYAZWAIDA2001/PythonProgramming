600d212b387058e6bbd515a39f4b8d2fd2bf78f6
INAMUL_FAYAZ_WAIDA15 <inamfw15@gmail.com>
2024-09-17 00:55:50 +0530

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,15 @@
+tup1=(1,2,3,4,5,6,7)
+print(tup1)
+
+country = ("Spain","India","Pakistan")
+print(country)
+print(country[1])
+print(country[-2])
+if "India" in country:
+    print("India is present")
+else:
+    print("India is absent")
+
+
+#same as that of lists
+
