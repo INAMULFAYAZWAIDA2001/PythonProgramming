2c492857bb1fe362a58f9129069897b894c5c5cc
INAMUL_FAYAZ_WAIDA15 <inamfw15@gmail.com>
2024-09-18 23:12:45 +0530

    Create FORwithELSE.py

---
+++ FORwithELSE.py
@@ -0,0 +1,14 @@
+# for x in range(10):
+#     print("iteration no.{} in FOR loop" .format(x+1))
+# else:
+#     print("else block in loop")
+# print("Out of Loop")
+
+i=0 
+while i<7:
+    if i==4:
+        break
+    print(i)
+    i=i+1
+else:
+    print('Sorry, no i')
