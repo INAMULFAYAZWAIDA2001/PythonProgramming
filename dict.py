d0625f203b90b08fdb2e7684bf8ad2af58820722
INAMUL_FAYAZ_WAIDA15 <inamfw15@gmail.com>
2024-09-18 23:12:40 +0530

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,10 @@
+info = {'name' : 'Inaam','age' : 23, 'eligible' : True}
+print(info)
+
+print(info['name'])
+print(info.get('age'))
+
+print(info.keys())
+print(info.values())
+
+print(info.items())
