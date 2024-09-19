a873a6b76a60bf2197cd71b435afcfcad26e392b
INAMUL_FAYAZ_WAIDA15 <inamfw15@gmail.com>
2024-09-19 23:28:29 +0530

    Create enumerate.py

---
+++ enumerate.py
@@ -0,0 +1,16 @@
+fruits = ['Apple','Banana','Mango']
+# for index, fruit in enumerate(fruits):
+
+
+for index, fruit in enumerate(fruits):
+# print(index,fruit)
+    print(f'{index+1}:{fruit}')
+
+colors = ('red','green','purple')
+for index, color in enumerate(colors):
+    print(f'{index+1}:{color}')
+
+
+d = 'Hello'
+for index,c in enumerate(d):
+    print(f'{index+1}:{c}')
